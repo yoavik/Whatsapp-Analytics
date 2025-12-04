e22c79226dd8a0629f752200f7f0f4ea4f6bdc8a
yoavik <yoav@itonut.org.il>
2025-12-04 14:59:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 const axios = require('axios');
 
-const WEBHOOK_URL = "PASTE_WEBHOOK_URL_HERE";
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
 wppconnect
   .create({
     session: 'monitor-session',
     puppeteerOptions: {
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      headless: true
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--disable-software-rasterizer',
+        '--single-process',
+        '--no-zygote',
+        '--disable-extensions',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-breakpad',
+        '--disable-default-apps',
+        '--disable-features=TranslateUI',
+        '--mute-audio',
+      ]
     }
   })
   .then((client) => start(client))
-  .catch((error) => console.log(error));
+  .catch((error) => console.error("WPPConnect init error:", error));
 
 function start(client) {
+  console.log("📡 WhatsApp monitor is running…");
+
   client.onMessage(async (msg) => {
-    if (msg.isGroupMsg) {
-      axios.post(WEBHOOK_URL, {
-        sender: msg.sender?.pushname || msg.sender?.id || "",
-        message: msg.body || "",
-        chatId: msg.chat.id
-      }).catch(err => console.log("Webhook error:", err));
+    try {
+      if (msg.isGroupMsg) {
+        await axios.post(WEBHOOK_URL, {
+          sender: msg.sender?.pushname || msg.sender?.id || "",
+          message: msg.body || "",
+          chatId: msg.chat.id
+        });
+
+        console.log("✔ sent to webhook:", msg.body);
+      }
+    } catch (err) {
+      console.error("Webhook error:", err.message);
     }
   });
 }
